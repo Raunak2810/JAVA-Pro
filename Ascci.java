fadd6e5089ecc29f9d5a821af249bdba6cafaf92
Raunak Mishra <raunakmishrabxr@gmail.com>
2026-03-25 23:47:15 +0530

    Add files via upload

---
+++ Ascci.java
@@ -0,0 +1,9 @@
+//11. Write a program to print the ASCII values
+class Ascci{
+    public static void main(){
+        char c;
+        for (c=1;c<=157;c++){
+            System.out.println((char)c);
+        }
+    }
+}
---
+++ Factorial.java
@@ -0,0 +1,12 @@
+//12. Write a program to find the factorial value of any number
+class Factorial{
+    public static void main(String[]args){
+        System.sc = 
+        int fact=1;
+        int n;
+        System.out.print("enter the value of n"+n);
+        for(int i=1;i<=n;i++)
+        fact*=i;
+        System.out.println("factorial of is:"+fact);
+    }
+}
